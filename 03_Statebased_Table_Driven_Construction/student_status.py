11b33dce1f9ee72775307171d72b2970fbb9c812
muhamad faris <m.faris5670@gmail.com>
2025-03-11 15:15:35 +0700

    pp

--- 03_Statebased_Table_Driven_Construction/student_status.py
+++ 03_Statebased_Table_Driven_Construction/student_status.py
@@ -26,3 +26,14 @@ state_transitions = {
         triggerinputstate.mengajukan_cuti: studentsstatusstate.Cuti
     }
 }
+def change_state(current_state, trigger_input):
+    cond_1 = current_state in state_transitions
+    cond_2 = trigger_input in state_transitions[current_state]
+    if cond_1 and cond_2:
+        return state_transitions[current_state][trigger_input]
+    return "transition tidak valid"
+
+current_state = studentsstatusstate.Terdaftar
+triggerinput = triggerinputstate.mengajukan_cuti
+next_state = change_state(current_state, triggerinput)
+print(next_state) #studentsstatusstate.Cuti

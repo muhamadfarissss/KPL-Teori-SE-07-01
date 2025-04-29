8a055d7f6825be4332d1172ce33fef860a471ffa
muhamad faris <m.faris5670@gmail.com>
2025-04-29 15:07:50 +0700

    09

---
+++ 09_Code_Reuse_and_Libraries/simpe_calculator/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='simple_calculator',
+    version='0.1',
+    packages = find_packages(),
+    description='A simple calculator package',
+    author='Your Name',
+    author_email='@pppp.com',
+)
---
+++ 13_Clean_Code_Standard/test.py
@@ -0,0 +1,4 @@
+import simple_calculator
+
+print (simple_calculator.add(1, 2))  # Output: 3
+print (simple_calculator.square(2))  # Output: 2
